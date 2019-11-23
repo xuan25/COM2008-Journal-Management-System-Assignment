a57f87bc9e089633ccb88c7dd666f322d828064a
瑄 <shanboxuan@me.com>
2019-11-23 20:06:07 +0000

    Update test class

--- Client/JournalManagementSystem/src/main/java/com/com2008/journalmanagementsystem/util/database/Account.java
+++ Client/JournalManagementSystem/src/main/java/com/com2008/journalmanagementsystem/util/database/Account.java
@@ -1,18 +1,20 @@
 package com.com2008.journalmanagementsystem.util.database;
 
 // A demo class of IDataRow
-public class Author implements IDataRow{
-    private String authorEmail;
+public class Account implements IDataRow{
+    private String email;
+    private String title;
     private String forename;
     private String surname;
     private String university;
 
-    public Author(){
+    public Account(){
         
     }
 
-    public Author(String authorEmail, String forename, String surname, String university){
-        this.authorEmail = authorEmail;
+    public Account(String email, String title, String forename, String surname, String university){
+        this.email = email;
+        this.title = title;
         this.forename = forename;
         this.surname = surname;
         this.university = university;
